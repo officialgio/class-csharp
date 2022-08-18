50f8f4a1c382cd33aeec9372c0848552f4c2e8b6
officialgio121 <officialgio121@gmail.com>
2022-08-18 11:05:37 -0500

    Added IncrementAndDecrementOperators

---
+++ MoreLogic/MoreLogic/IncremntAndDecrementOperators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoreLogic;
+
+public class IncremntAndDecrementOperators
+{
+	static void Main()
+	{
+        int i = 1;
+        //int j = i++; uncomment for i++
+        int j = ++i; // uncomment if you're going to use line 10
+
+        Console.WriteLine("i++: j = {0}, i = {1}", j, i);
+        Console.WriteLine("++i: j = {0}, i = {1}", j, i);
+    }
+}
