60ffa8a5d38fa725b8994ad610077d3530668213
officialgio121 <officialgio121@gmail.com>
2022-08-23 23:25:43 -0500

    Added class and objects

---
+++ ClassesAndObjects/ClassesAndObjects/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ClassesAndObjects;
+
+public class Person
+{
+	// properties
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+
+	// constructor that takes a FirstName and LastName
+	public Person(string firstName, string lastName)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+	}
+
+    public override string ToString()
+    {
+		return $"Person[Name: {FirstName}, LastName: {LastName}]" ;
+    }
+}
---
+++ ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassesAndObjects;
+
+public class Program
+{
+    static void Main()
+    {
+        // instantiate many objects with diffrent names
+        Person personOne = new Person("Giovanny", "Hernandez");
+        Person personTwo = new Person("Name1", "LastName1");
+        Person personThree = new Person("Name2", "LastName2");
+        Person personFour = new Person("Name3", "LastName3");
+        Person personFive = new Person("Name4", "LastName4");
+        Person personSix = new Person("Name5", "LastName5");
+        Person personSeven = new Person("Name6", "LastName6");
+        Person personEight = new Person("Name7", "LastName7");
+        Person personNine = new Person("Name8", "LastName8");
+
+        // store them manually in an array 
+        // usually these are store with an array
+        // but because we've only cover arrays this is sort of the same process
+
+        // usually they're store in databases along with many more properties than just a name and lastName 
+        Person[] arrayOfPeople = new Person[9]
+        {
+            personOne,
+            personTwo,
+            personThree,
+            personFour,
+            personFive,
+            personSix,
+            personSeven,
+            personEight,
+            personNine
+        };
+
+        // print
+        for (int i = 0; i < arrayOfPeople.Length; i++)
+        {
+            Console.WriteLine(arrayOfPeople[i]);
+        }
+    }
+}
